6c0ba5666dba0ced3151a2f57b015683942f7f9d
xingfangfa41-png <xingfangfa41@gmail.com>
2026-05-24 12:48:57 +0800

    trigger: force Vite rebuild

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { HashRouter } from "react-router";
 import "./index.css";
+// force-rebuild: v2
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
